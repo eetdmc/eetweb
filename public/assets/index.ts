f6461714f25786ef074f1034a56329d0b6caabbf
rahees <raheez1995india@gmail.com>
2025-09-18 17:14:18 +0530

    hero bottom section and service section

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,4 +2,5 @@ import logo from "./images/logo.svg";
 import user from "./images/icons/user.svg";
 import pattern1 from "./images/pattern-1.svg";
 import arrowTopRight from "./images/home/hero/arrow-top-right.svg";
-export const assets = { logo, user, pattern1, arrowTopRight };
+import arrowPrimary from "./images/icons/tr-arrow-primary.svg";
+export const assets = { logo, user, pattern1, arrowTopRight, arrowPrimary };
