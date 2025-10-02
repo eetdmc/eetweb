2fd73621d8227c683973dcf0ec09b347d7f2f57a
rahees <raheez1995india@gmail.com>
2025-10-02 18:24:49 +0530

    our team

---
+++ app/(user)/our-team/page.tsx
@@ -0,0 +1,10 @@
+import Index from "@/components/our-team";
+const Page = () => {
+  return ( 
+    <>
+    <Index />
+    </>
+   );
+}
+ 
+export default Page;
---
+++ app/components/our-team/data.ts
@@ -0,0 +1,25 @@
+export const teamData = {
+  teamList: [
+    {
+      name: "Name Dummy Text",
+      position: "Position Dummy Text",
+      image: "/assets/images/team/emp-1.png",
+    },
+    {
+      name: "Name Dummy Text",
+      position: "Position Dummy Text",
+      image: "/assets/images/team/emp-2.png",
+    },
+    {
+      name: "Name Dummy Text",
+      position: "Position Dummy Text",
+      image: "/assets/images/team/emp-3.png",
+    },
+    {
+      name: "Name Dummy Text",
+      position: "COO",
+      image: "/assets/images/team/emp-1.png",
+    },
+  ],
+};
+    
