15a024a916bde42192bab5e894b44e3fd6d13c10
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-23 11:42:38 +0530

    feat(about-us: api connection done)

--- app/(user)/about-us/page.tsx
+++ app/(user)/about-us/page.tsx
@@ -1,12 +1,10 @@
 import Index from "@/app/components/about-us";
 
 const Page = async () => {
-  console.log(process.env.BASE_URL);
   const response = await fetch(`${process.env.BASE_URL}/api/admin/about`, {
     next: { revalidate: 60 },
   });
   const data = await response.json();
-  console.log(data);
   return (
     <>
       <Index data={data.data} />
