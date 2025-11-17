f209ec600422206d68671e103edd8299c763a215
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-11-17 18:06:24 +0530

    fix(responsiveness, font-size, spacing)

--- app/components/Services/sections/ServicesList.tsx
+++ app/components/Services/sections/ServicesList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 export interface ServiceItem {
   title: string;
@@ -10,28 +10,27 @@ export interface ServiceItem {
   image: string;
 }
 
+const slideLeft: Variants = {
+  hidden: { opacity: 0, x: -80 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.8, ease: [0.25, 0.1, 0.25, 1] },
+  },
+};
+
+const slideRight: Variants = {
+  hidden: { opacity: 0, x: 80 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.8, ease: [0.25, 0.1, 0.25, 1] },
+  },
+};
+
 const ServicesSection = ({ servicesData }: { servicesData: ServiceItem[] }) => {
-  // Motion variants
-  const slideLeft = {
-    hidden: { opacity: 0, x: -80 },
-    show: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 0.8, ease: "easeOut" },
-    },
-  };
-
-  const slideRight = {
-    hidden: { opacity: 0, x: 80 },
-    show: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 0.8, ease: "easeOut" },
-    },
-  };
-
   return (
-    <section className="w-full pt-10 pb-35 md:pt-16 md:pb-40 lg:pt-20 lg:pb-60 2xl:pt-[100px] 2xl:pb-[315px]">
+    <section className="w-full pt-10 pb-35 md:pt-16 md:pb-40 lg:pt-20 lg:pb-60  2xl:pt-[100px] 2xl:pb-[315px]">
       <div className="container flex flex-col gap-[50px] xl:gap-[100px]">
         {servicesData.map((item, index) => {
           const isImageLeft = index % 2 === 0;
@@ -43,7 +42,7 @@ const ServicesSection = ({ servicesData }: { servicesData: ServiceItem[] }) => {
               variants={animationVariant}
               initial="hidden"
               whileInView="show"
-              viewport={{ once: true, amount: 0.3 }} // triggers earlier than GSAP
+              viewport={{ once: true, amount: 0.3 }}
               className={`flex flex-col xl:flex-row items-center opacity-0 gap-[20px] lg:gap-[40px] xl:gap-[70px] ${
                 isImageLeft ? "" : "xl:flex-row-reverse"
               }`}
@@ -59,14 +58,12 @@ const ServicesSection = ({ servicesData }: { servicesData: ServiceItem[] }) => {
                 />
               </div>
 
-              {/* TEXT CONTENT */}
+              {/* TEXT */}
               <div className="flex flex-col gap-[15px] lg:gap-[30px] text-black font-light">
                 <h2 className="text-50 font-sans leading-[1]">{item.title}</h2>
-
                 <p className="text-19 font-inter leading-[1.526315789473684]">
                   {item.desc1}
                 </p>
-
                 <p className="text-19 font-inter leading-[1.526315789473684]">
                   {item.desc2}
                 </p>
