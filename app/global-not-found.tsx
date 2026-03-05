db5c067da2257327b984ad874d65d2a69aa125c2
rahees <raheez1995india@gmail.com>
2026-03-05 12:12:12 +0530

    build fix

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import Header from "./components/commonV2/Header";
 import Footer from "./components/commonV2/Footer";
 import "./globals.css";
